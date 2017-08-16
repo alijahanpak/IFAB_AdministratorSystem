a0ced3659fbb7a5330f702daf5ea829df00f1367
Ali Jahanpak <a.jahanpak@gmail.com>
2017-08-16 09:46:41 +0430

    update deprived_area 25-5-96

--- Modules/Budget/Resources/views/pages/deprived_area.blade.php
+++ Modules/Budget/Resources/views/pages/deprived_area.blade.php
@@ -1,5 +1,58 @@
 @extends('budget::layouts.master')
 @section('content')
+    <div style="z-index: 9999;" class="tiny reveal" id="modalInsert" data-reveal>
+        <div class="modal-margin">
+            <form>
+                <div class="grid-x">
+                    <div class="medium-6 cell dashboard-padding">
+                        <label>شهرستان
+                            <select>
+                                <option value="husker">همدان</option>
+                                <option value="starbuck">ملایر</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div class="medium-6 cell dashboard-padding">
+                        <label>بخش
+                            <select disabled>Disabled>
+                                <option value="husker">همدان</option>
+                                <option value="starbuck">ملایر</option>
+                            </select>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="grid-x">
+                    <div class="medium-6 cell dashboard-padding">
+                        <label>شهرستان
+                            <select>
+                                <option value="husker">همدان</option>
+                                <option value="starbuck">ملایر</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div class="medium-6 cell dashboard-padding">
+                        <label>بخش
+                            <select disabled>Disabled>
+                                <option value="husker">همدان</option>
+                                <option value="starbuck">ملایر</option>
+                            </select>
+                        </label>
+                    </div>
+                </div>
+                <div class="medium-6 columns">
+                    <label>شرح
+                        <textarea style="min-height: 150px;"></textarea>
+                    </label>
+                </div>
+            </form>
+        </div>
+        <button class="close-button" data-close aria-label="Close modal" type="button">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <button type="button" class="my-secondary button float-left" data-open="exampleModal1"> ثبت </button>
+    </div>
+
     <div style="padding: 30px;" class="medium-10 border-right-line">
         <div style="padding-right: 15px;padding-left: 15px;" class="grid-x">
             <div class="medium-12">
@@ -18,12 +71,18 @@
         </div>
 
         <div class="grid-x my-grid-margin">
-            <div class="medium-4 cell dashboard-padding">
-                <!--<button type="button" class="my-secondary button tiny float-left">اطلاعات بیشتر</button>-->
-                <div class="input-group">
-                    <input class="input-group-field small-font float-left" type="text">
-                    <div class="input-group-button">
-                        <button type="button" class="primary button"><i class="fi-magnifying-glass"></i></button>
+            <div class="medium-12 dashboard-padding">
+
+                <div class="clearfix">
+                    <button type="button" class="my-secondary button small" data-open="modalInsert">جدید</button>
+                    <div class="float-left">
+                        <div class="input-group float-left">
+                            <input class="input-group-field small-font" type="text">
+                            <div class="input-group-button">
+                                <button type="button" class="primary button"><i class="fi-magnifying-glass"></i></button>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -43,7 +102,7 @@
                     <div style="padding-right: 20px;padding-left: 20px;" class="medium-12">
                         <div class="clearfix border-btm-line">
                             <div class="button-group float-left">
-                                <a style="cursor: pointer;" class="clear button" type="button" data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="ایجاد نمودار" data-position="top" data-alignment="center">
+                                <a style="cursor: pointer;" class="clear button" type="button" data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="ایجاد گزارش" data-position="top" data-alignment="center">
                                     <i style="color: #01aef0;" class="fi-page-edit size-30"></i>
                                 </a>
                             </div>
@@ -108,19 +167,24 @@
                         </div>
                     </div>
                     <div class="tabs-panel" id="panel2">
-
+                        <p>two</p>
+                        <img class="thumbnail" src="assets/img/generic/rectangle-7.jpg">
                     </div>
                     <div class="tabs-panel" id="panel3">
-
+                        <p>three</p>
+                        <p>Check me out! I'm a super cool Tab panel with text content!</p>
                     </div>
                     <div class="tabs-panel" id="panel4">
-
+                        <p>four</p>
+                        <img class="thumbnail" src="assets/img/generic/rectangle-2.jpg">
                     </div>
                     <div class="tabs-panel" id="panel5">
-
+                        <p>five</p>
+                        <p>Check me out! I'm a super cool Tab panel with text content!</p>
                     </div>
                     <div class="tabs-panel" id="panel6">
-
+                        <p>six</p>
+                        <img class="thumbnail" src="assets/img/generic/rectangle-8.jpg">
                     </div>
                 </div>
             </div>
