f93428a5ae5452dee039a87ecdf5cd31a3fbc6cc
morteza amzajerdi <ammorteza@yahoo.com>
2017-08-19 14:59:44 +0430

    fixed some bug in login form 1396/5/28

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,7 +26,6 @@
     <div class="grid-x">
         <div style="margin-top: 20px;" class="large-4 large-offset-4 cell callout ">
             {!! Form::open(array('id' => 'signInForm' , 'url' => '/signin' , 'class' => 'form' , 'data-abide novalidate')) !!}
-                {!! csrf_field() !!}
                 @if(count($errors->loginErrors))
                     <div class="alert callout">
                         <p class="BYekan login-alert"><i class="fi-alert"></i> {{ $errors->loginErrors->first() }}</p>
@@ -35,29 +34,27 @@
                 <div class="row">
                     <div class="small-12 columns">
                         <label>نام کاربری
-                            <input name="email" type="text" required pattern="text">
-                            <span class="form-error"></span>
+                            {{ Form::text('email', \Illuminate\Support\Facades\Input::old('email') , array( 'required' , 'pattern' => 'text')) }}
+                            <span class="form-error font-wei">نام کاربر فراموش شده است!</span>
                         </label>
                     </div>
                 </div>
                 <div class="row">
                 <div class="small-12 columns">
                     <label>رمز عبور
-                        <input name="password" type="password" id="password" required >
-                        <span class="form-error">لطفا رمز عبور را روباره وارد کنید</span>
+                        {{ Form::password('password' , array('required')) }}
+                        <span class="form-error font-wei">کلمه عبور فراموش شده است!</span>
                     </label>
                 </div>
                 </div>
                 <div class="row">
                     <fieldset class="large-6 columns">
-                        <button class="button expanded" type="submit" value="Submit">ورود</button>
+                        {{ Form::submit('ورود' , array('name' => 'signin' , 'class' => 'button expanded')) }}
                     </fieldset>
                 </div>
              {!! Form::close() !!}
         </div>
     </div>
-
-
 </div>
 <div class="grid-x">
     <div class="large-12">
@@ -71,5 +68,9 @@
         <p class="small-font footer-txt-size">تمامی حقوق این وب سایت متعلق یه اداره کل میراث فرهنگی صنایع دستی و گردشگری استان همدان می باشد</p>
     </div>
 </div>
+<script src="{{ asset('js/vendor/jquery.js') }}"></script>
+<script src="{{ asset('js/vendor/what-input.js') }}"></script>
+<script src="{{ asset('js/vendor/foundation.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
