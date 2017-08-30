79f0fddd16366d792bc0f0eae97e09738a233a4a
morteza amzajerdi <ammorteza@yahoo.com>
2017-08-30 08:41:18 +0430

    create custom scrollable table 1396/6/8

--- Modules/Budget/Resources/views/pages/credit_distribution/rows.blade.php
+++ Modules/Budget/Resources/views/pages/credit_distribution/rows.blade.php
@@ -12,55 +12,155 @@
         </div>
     </div>
     <div class="columns">
-        <table id="fixedTable" class="stripe row-border order-column nowrap" width="100%" cellspacing="0">
-            <thead>
-            <tr>
-                <th>ردیف توزیع اعتبار</th>
-                <th>شماره طرح</th>
-                <th>عنوان طرح</th>
-                <th>فصل بودجه</th>
-                <th>سرجمع</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Tiger</td>
-                <td>Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>Edinburgh</td>
-            </tr>
-            <tr>
-                <td>Tiger</td>
-                <td>Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>Edinburgh</td>
-            </tr>
-            <tr>
-                <td>Tiger</td>
-                <td>Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>Edinburgh</td>
-            </tr>
-            <tr>
-                <td>Tiger</td>
-                <td>Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>Edinburgh</td>
-            </tr>
-            <tr>
-                <td>Tiger</td>
-                <td>Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>Edinburgh</td>
-            </tr>
-
-
-            </tbody>
-        </table>
+        <div class="grid-x" style="margin-left: 10px; background-color: #E6E6E6">
+            <div class="medium-2 table-border">
+                fasdf
+            </div>
+            <div class="medium-10">
+                <div class="grid-x">
+                    <div class="medium-4 table-border">
+                        df
+                    </div>
+                    <div class="medium-4  table-border">
+                        ssdf
+                    </div>
+                    <div class="medium-4  table-border">
+                        sdf
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="overflow-y:scroll; height:300px; margin-bottom: 1rem; border-bottom: 1px solid #C7CDD1">
+            <div class="grid-x tableRowColor">
+                <div class="medium-2 table-border">
+                    fasdf
+                </div>
+                <div class="medium-10">
+                    <div class="grid-x">
+                        <div class="medium-4 table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            <a onclick="openTableRowAcc('rowTest')">sadfasdfgsdf</a>
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                    </div>
+                    <div class="grid-x table-border display-off" id="rowTest">
+                        <table>
+                            <thead>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                                <th>1231234</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                    <td>23452345</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="grid-x">
+                <div class="medium-2 table-border">
+                    dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                </div>
+                <div class="medium-10">
+                    <div class="grid-x">
+                        <div class="medium-4 table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                    </div>
+                    <div class="grid-x table-border">
+                        dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                    </div>
+                </div>
+            </div>
+            <div class="grid-x">
+                <div class="medium-2 table-border">
+                    fasdf
+                </div>
+                <div class="medium-10">
+                    <div class="grid-x">
+                        <div class="medium-4 table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                    </div>
+                    <div class="grid-x table-border">
+                        dfgsdfg
+                    </div>
+                </div>
+            </div>
+            <div class="grid-x">
+                <div class="medium-2 table-border">
+                    dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                </div>
+                <div class="medium-10">
+                    <div class="grid-x">
+                        <div class="medium-4 table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                        <div class="medium-4  table-border">
+                            ssdf
+                        </div>
+                        <div class="medium-4  table-border">
+                            dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                        </div>
+                    </div>
+                    <div class="grid-x table-border">
+                        dfsdfghdfgh dfgh dfghd fgh dfghd fghd fghd fghdfghd fghd fghd fghd fghd
+                    </div>
+                </div>
+            </div>
+            <div class="grid-x">
+                <div class="medium-2 table-border">
+                    fasdf
+                </div>
+                <div class="medium-10">
+                    <div class="grid-x">
+                        <div class="medium-4 table-border">
+                            df
+                        </div>
+                        <div class="medium-4  table-border">
+                            ssdf
+                        </div>
+                        <div class="medium-4  table-border">
+                            sdf
+                        </div>
+                    </div>
+                    <div class="grid-x table-border">
+                        dfgsdfg
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
