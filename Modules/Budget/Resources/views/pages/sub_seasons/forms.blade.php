b3a560e867a3bc4f2df19b4cc2dfff3b26436b94
Ali Jahanpak <a.jahanpak@gmail.com>
2017-09-14 09:47:39 +0430

    big fix 96-06-23

--- Modules/Budget/Resources/views/pages/sub_seasons/forms.blade.php
+++ Modules/Budget/Resources/views/pages/sub_seasons/forms.blade.php
@@ -187,4 +187,67 @@
     </button>
 </div>
 <!--Modal Insert End-->
+<!--Modal Update cost Start-->
+<div style="z-index: 9999;" class="tiny reveal" id="SSC_ModalInsert" data-reveal
+     data-animation-in="slide-in-down fast bounce" xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <div id="add-product" class="modal-margin small-font  padding-lr">
+        {!! Form::open(array('v-on:submit.prevent' => 'createProduct','id' => 'registerTSCForm' , 'url' => '' , 'class' => 'form' , 'data-abide novalidate')) !!}
+        {!! csrf_field() !!}
+        <div class="grid-x" id="sscexistErrorInRegForm" style="display: none">
+            <div class="medium-12 columns padding-lr">
+                <div class="alert callout">
+                    <p class="BYekan login-alert"><i class="fi-alert"></i> ریز فصل با این مشخصات قبلا ثبت شده است!</p>
+                </div>
+            </div>
+        </div>
+        <div class="grid-x">
+            <div class="medium-12 cell padding-lr">
+                <label>فصل
+                    <select name="sId" id="sscsId" required v-model="product.season">
+                        <option value=""></option>
+                        @foreach($seasons as $season)
+                            <option value="{{ $season->id }}">{{ $season->sSubject }}</option>
+                        @endforeach
+                    </select>
+                </label>
+                <span class="form-error error-font" data-form-error-for="sscsId">فصل را انتخاب کنید!</span>
+            </div>
+        </div>
+        <div class="grid-x">
+            <div class="medium-12 columns padding-lr">
+                <label>ریز فصل
+                    <input type="text" name="tsSubject" id="ssctsSubject" required pattern="text" v-model="product.subSeason">
+                </label>
+                <span class="form-error error-font" data-form-error-for="ssctsSubject">ریز فصل فراموش شده است!</span>
+            </div>
+        </div>
+        <div class="grid-x">
+            <div class="small-12 columns padding-lr">
+                <label>شرح
+                    <textarea name="tsDescription" id="ssctsDescription" style="min-height: 150px;" v-model="product.description"></textarea>
+                </label>
+            </div>
+        </div>
+        <div class="medium-6 columns padding-lr">
+            <button name="Submit" class="my-secondary button float-left btn-for-load"> <span class="btn-txt-mrg">ثبت</span>    <i id="registerSubmitActivityCircle">
+                    <div class="la-line-spin-clockwise-fade-rotating la-sm float-left">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </i>
+            </button>
+        </div>
+        {!! Form::close() !!}
+    </div>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<!--Modal Update Cost End-->
 
