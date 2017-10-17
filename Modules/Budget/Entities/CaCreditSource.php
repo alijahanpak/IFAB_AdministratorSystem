5b62726352d561f05b0076206558ac7f490cc6fc
morteza amzajerdi <ammorteza@yahoo.com>
2017-10-17 09:17:46 +0330

    create Cost Agreement and Ca Credit Source table and models 1396/7/25

---
+++ Modules/Budget/Entities/CaCreditSource.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Budget\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CaCreditSource extends Model
+{
+    protected $table = 'tbl_ca_credit_source';
+    protected $fillable = [];
+}
---
+++ Modules/Budget/Entities/CostAgreement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Budget\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CostAgreement extends Model
+{
+    protected $table = 'tbl_cost_agreement';
+    protected $fillable = [];
+}
