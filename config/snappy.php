88a6252a8854a6699564e925efb77e29902638f9
morteza amzajerdi <ammorteza@yahoo.com>
2017-10-21 14:23:09 +0330

    instal wkhtmltopdf i386  in laravel 1396/7/29

--- config/snappy.php
+++ config/snappy.php
@@ -6,6 +6,7 @@ return array(
     'pdf' => array(
         'enabled' => true,
         'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //'binary'  => base_path('vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
